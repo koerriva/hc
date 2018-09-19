68598c284ee721e535f97e3b853ac60fea3fadba
koerriva <rivakoer@gmail.com>
2018-09-19 23:35:17 +0800

    #数组初探(二)

--- src/Lang/IRBuilder.hs
+++ src/Lang/IRBuilder.hs
@@ -7,7 +7,7 @@ import Data.Text.Lazy.IO as T
 
 import LLVM.Pretty  -- from the llvm-hs-pretty package
 import LLVM.AST hiding (function)
-import LLVM.AST.Type as AST
+import LLVM.AST.Type as Type
 import qualified LLVM.AST.Float as F
 import qualified LLVM.AST.Constant as C
 
@@ -15,10 +15,26 @@ import LLVM.IRBuilder.Module
 import LLVM.IRBuilder.Monad
 import LLVM.IRBuilder.Instruction
 
-simple :: IO ()
-simple = T.putStrLn $ ppllvm $ buildModule "exampleModule" $ mdo
+import Debug.Trace
+import qualified LLVM.AST as AST
 
-  function "add" [(i32, "a"), (i32, "b")] i32 $ \[a, b] -> mdo
-    entry <- block `named` "entry"; do
-      c <- add a b
-      ret c
+import Lang.JIT
+
+simple :: IO ()
+simple = do
+ T.putStrLn $ ppllvm codegen
+ runJIT codegen "add"
+
+codegen :: AST.Module
+codegen = do
+  buildModule "test" $ mdo
+    function "add" [(i32, "a"), (i32, "b")] i32 $ \[a, b] -> mdo
+      entry <- block `named` "entry"; do
+        let arrayTy = Type.ArrayType 8 Type.i8
+            idx0 = AST.ConstantOperand (C.Int 32 0)
+            idx1 = AST.ConstantOperand (C.Int 32 7)
+        ptr <- alloca arrayTy Nothing 0
+        ep <- gep ptr [idx0,idx1]
+        store ep 0 (AST.ConstantOperand (C.Int 8 97))
+        e <- load ep 0
+        ret e
